e9b6881ce9405b2c0dc9dca1d752664e31ee579c
rndals0521 <rndals0521@naver.com>
2021-04-05 18:13:50 +0900

    040506:13

---
+++ src/com/ict05/collection/Ex01.java
@@ -0,0 +1,16 @@
+package com.ict05.collection;
+
+public class Ex01 {
+	// 제네릭과 컬랙션 (자료구조)
+	// 제네릭 : 컬랙션이 어떤 객체들로 이루어졌는지를 표시하는 객체 타입을 말한다.
+	// 컬랙션 : 객체들을 모아서 관리하는 인터페이스를 구현한 것. (자바에서)
+	// API    : <T> => 객체 타입, <E> => 요소 (컬랙션 안에 존재하는 객체 하나를 뜻한다.)
+	//			Map 형식에서는 <K,V> => Key(키), Value(값)
+	//          key와 value가 1:1 대응, Key를 호출하면 Value가 나온다.
+	// 형식   : 컬랙션<제네릭>
+	// 최상위 컬랙션은 Collection<E>, Map<K,V>
+	// Collection<E>를 상속받은 인터페이스는 : Set<E> ,List<E>, Queue<E> 
+	
+	// Collection의 주요 메소드
+	// 
+}
